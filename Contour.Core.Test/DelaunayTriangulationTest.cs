fb49b1efe7760e63d5bbf0fb18bed71603e9c20d
Ed Walton <ed.dan.walton@gmail.com>
2025-01-04 13:34:08 +0000

    Created Contour.Core and test project. Wrote first failing (and non-compiling test)

---
+++ Contour.Core.Test/DelaunayTriangulationTest.cs
@@ -0,0 +1,23 @@
+
+using System.Drawing;
+
+namespace Contour.Core.Test
+{
+    public class DelaunayTriangulationTest
+    {
+        [Fact]
+        public void CreatesSingleTriangleAround3DistinctPoints()
+        {
+            //Arrange
+            var testPoints = new List<Point> { new Point(0.0, 0.0), new Point(0.0, 1.0), new Point(1.0, 1.0) };
+
+            var triangulation = new DelaunayTriangulation(testPoints);
+
+            //Act
+            var triangles = triangulation.Triangulate();
+
+            //Assert
+            Assert.Equal(new List<Triangle> { new Triangle(testPoints) }, triangles);
+        }
+    }
+}
---
+++ Contour.Core/DelaunayTriangulation.cs
@@ -0,0 +1,7 @@
+﻿namespace Contour.Core
+{
+    public class DelaunayTriangulation
+    {
+
+    }
+}
