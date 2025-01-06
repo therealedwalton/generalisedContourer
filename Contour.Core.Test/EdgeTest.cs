759e7fad84842aa0235e8e1013712af090163ae4
Ed Walton <ed.dan.walton@gmail.com>
2025-01-06 20:45:15 +0000

    Created Edge class and got EdgeExhibitValueEquality to green with a few cases.

---
+++ Contour.Core.Test/EdgeTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contour.Core.Test
+{
+    public class EdgeTest
+    {
+        [Theory]
+        [MemberData(nameof(EquivalentEdges))]
+        public void EdgeExhibitValueEquality(Edge edge1, Edge edge2, bool expectedSame)
+        {
+            //Act
+            bool equal = edge1.Equals(edge2);
+
+            //Assert
+            Assert.Equal(expectedSame, equal);
+        }
+
+        public static IEnumerable<object[]> EquivalentEdges =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new Edge(new Point(0.0, 0.0), new Point(1.0, 1.0)),
+                    new Edge(new Point(0.0, 0.0), new Point(1.0, 1.0)),
+                    true
+                },
+                new object[]
+                {
+                    new Edge(new Point(0.0, 0.0), new Point(1.0, 1.0)),
+                    new Edge(new Point(1.0, 1.0), new Point(0.0, 0.0)),
+                    true
+                },
+            };
+    }
+}
---
+++ Contour.Core/Edge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contour.Core
+{
+    public class Edge
+    {
+        public Point Start { get; }
+        public Point End { get; }
+
+        public Edge(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Edge other)
+            {
+                return (Start.Equals(other.Start) && End.Equals(other.End)) ||
+                       (Start.Equals(other.End) && End.Equals(other.Start));
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Start.x, Start.y, End.x, End.y);
+        }
+    }
+}
