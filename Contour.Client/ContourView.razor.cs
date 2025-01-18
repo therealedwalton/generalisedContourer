e0eac4eee518dc358c8114c280e94b4b3b6b79f1
Ed Walton <ed.dan.walton@gmail.com>
2025-01-18 11:46:01 +0000

    Made ShouldDisplayASeriesOfPoints green with obvious solution.

---
+++ Contour.Client/ContourView.razor.cs
@@ -0,0 +1,16 @@
+﻿using Contour.Core;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contour.Client
+{
+    public partial class ContourView
+    {
+        [Parameter]
+        public List<Point> Points { get; set; } = new List<Point>();
+    }
+}
