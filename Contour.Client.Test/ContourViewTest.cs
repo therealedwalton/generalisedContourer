35117813d2f36294041f18cf5d90e2ac730aa662
Ed Walton <ed.dan.walton@gmail.com>
2025-01-18 20:09:12 +0000

    Wrote failing test ShouldDisplayTriangulationOfPoints.

--- Contour.Client.Test/ContourViewTest.cs
+++ Contour.Client.Test/ContourViewTest.cs
@@ -5,7 +5,7 @@ using Contour.Core;
 
 namespace Contour.Client.Test
 {
-    public class IconComponentTests : TestContext
+    public class ContourViewTests : TestContext
     {
         [Fact]
         public void ShouldRenderSvg()
@@ -32,7 +32,6 @@ namespace Contour.Client.Test
                 new Point(45, 64)
             };
 
-
             var cut = RenderComponent<ContourView>(parameters => parameters.Add(p => p.Points, points));
 
             // Act
@@ -45,6 +44,39 @@ namespace Contour.Client.Test
             {
                 Assert.Equal(points[i].x, double.Parse(circles[i].GetAttribute("cx")));
                 Assert.Equal(points[i].y, double.Parse(circles[i].GetAttribute("cy")));
+            }
+        }
+
+        [Fact]
+        public void ShouldDisplayTriangulationOfPoints()
+        {
+            // Arrange
+            var testPoints = new List<Point> { new Point(0.0, 0.0), new Point(0.0, 1.0), new Point(1.0, 1.0), new Point(1.0, 0.0) };
+            var triangles = new List<Triangle>
+            {
+                new Triangle(testPoints[0], testPoints[1], testPoints[3]),
+                new Triangle(testPoints[1], testPoints[2], testPoints[3])
+            };
+
+            var cut = RenderComponent<ContourView>(parameters => parameters.Add(p => p.Points, testPoints));
+
+            // Act
+            var lines = cut.FindAll("line");
+
+            // Assert
+            Assert.Equal(triangles.Count * 3, lines.Count);
+
+            for (int i = 0; i < triangles.Count; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Assert.Equal(triangles[i].Vertices[j].x, double.Parse(lines[i * 3 + j].GetAttribute("x1")));
+                    Assert.Equal(triangles[i].Vertices[j].y, double.Parse(lines[i * 3 + j].GetAttribute("y1")));
+
+                    var upperIndex = j + 1 > 2 ? 0 : j + 1;
+                    Assert.Equal(triangles[i].Vertices[upperIndex].x, double.Parse(lines[i * 3 + upperIndex].GetAttribute("x2")));
+                    Assert.Equal(triangles[i].Vertices[upperIndex].y, double.Parse(lines[i * 3 + upperIndex].GetAttribute("y2")));
+                }
             }
         }
     }
