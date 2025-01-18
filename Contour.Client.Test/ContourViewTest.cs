15093524d47aa7fa68363bdf5b8f215a1042c5ef
Ed Walton <ed.dan.walton@gmail.com>
2025-01-18 11:38:55 +0000

    Wrote failing test ShouldDisplayASeriesOfPoints.

--- Contour.Client.Test/ContourViewTest.cs
+++ Contour.Client.Test/ContourViewTest.cs
@@ -1,5 +1,7 @@
+using AngleSharp.Dom;
 using Bunit;
 using Contour.Client;
+using Contour.Core;
 
 namespace Contour.Client.Test
 {
@@ -16,6 +18,34 @@ namespace Contour.Client.Test
 
             // Assert
             Assert.NotNull(svg);
+        }
+
+        [Fact]
+        public void ShouldDisplayASeriesOfPoints()
+        {
+            // Arrange
+            var points = new List<Point>()
+            {
+                new Point(5, 5),
+                new Point(20, 14),
+                new Point(78, 90),
+                new Point(45, 64)
+            };
+
+
+            var cut = RenderComponent<ContourView>(parameters => parameters.Add(p => p.Points, points));
+
+            // Act
+            var circles = cut.FindAll("circle");
+
+            // Assert
+            Assert.Equal(points.Count, circles.Count);
+
+            for (int i = 0; i < points.Count; i++) 
+            {
+                Assert.Equal(points[i].x, double.Parse(circles[i].GetAttribute("cx")));
+                Assert.Equal(points[i].y, double.Parse(circles[i].GetAttribute("cy")));
+            }
         }
     }
 }
